0dade5e74566dfb47dbfe6249207e9257505768d
Pannu786 <thepannu786@gmail.com>
2022-03-21 22:38:46 +0530

    meh :/ dne

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+import { Table } from './Table';
+
+import { Users } from './user';
 
 function App() {
+  const [query, setQuery] = useState('');
+
+  const keys = ['first_name', 'last_name', 'email'];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
+
   return (
     <div className='app'>
-      <input type='text' placeholder='Search...' className='search' />
-      <ul className='list'>
-        <li className='listItem'>Mike</li>
-        <li className='listItem'>Nike</li>
-        <li className='listItem'>Sike</li>
-      </ul>
+      <input
+        type='text'
+        placeholder='Search...'
+        className='search'
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Table data={search(Users)} />
     </div>
   );
 }
