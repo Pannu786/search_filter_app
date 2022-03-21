fd718b2ee1cb79cfa765805085ffe755d6e3dfe8
Pannu786 <thepannu786@gmail.com>
2022-03-21 20:55:17 +0530

    imported css file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='app'>
+      <input type='text' placeholder='Search...' className='search' />
+      <ul className='list'>
+        <li className='listItem'>Mike</li>
+        <li className='listItem'>Nike</li>
+        <li className='listItem'>Sike</li>
+      </ul>
     </div>
   );
 }
